c3bd2800190a959a082642fa6eb9a98ad014d83d
Sevil Coskun <sevilcoskun7@gmail.com>
2019-11-14 18:24:05 +0100

    fixed the issue #2

---
+++ QuestionSet.js
@@ -0,0 +1,174 @@
+var game = JSON.parse(`{
+    "questions": [
+        {
+            "text": "1-What is the name of the German airline?",
+            "options": [
+                {
+                    "text": "Luftansa",
+                    "correct": true
+                },
+                {
+                    "text": "Aitalia",
+                    "correct": false
+                },
+                {
+                    "text": "Turkish Airlines",
+                    "correct": false
+                }
+            ]
+        },
+        {
+            "text": "2-In which European city can you find the home of Anne Frank?",
+            "options": [
+                {
+                    "text": "Paris",
+                    "correct": false
+                },
+                {
+                    "text": "Berlin",
+                    "correct": false
+                },
+                {
+                    "text": "Amsterdam",
+                    "correct": true
+                }
+            ]
+        },
+        {
+            "text": "3-How many stars has the American flag got?",
+            "options": [
+                {
+                    "text": "15",
+                    "correct": false
+                },
+                {
+                    "text": "50",
+                    "correct": true
+                },
+                {
+                    "text": "25",
+                    "correct": false
+                }
+            ]
+        },
+        {
+            "text": "4-How many oscars did the Titanic movie got?",
+            "options": [
+                {
+                    "text": "11",
+                    "correct": true
+                },
+                {
+                    "text": "10",
+                    "correct": false
+                },
+                {
+                    "text": "17",
+                    "correct": false
+                }
+            ]
+        },
+        {
+            "text": "5-Which animal is on the golden Flemish flag?",
+            "options": [
+                {
+                    "text": "Lion",
+                    "correct": true
+                },
+                {
+                    "text": "Parrot",
+                    "correct": false
+                },
+                {
+                    "text": "Elephant",
+                    "correct": false
+                }
+            ]
+        },
+        {
+            "text": "6-How many Tomb Raider movies were made?",
+            "options": [
+                {
+                    "text": "3",
+                    "correct": false
+                },
+                {
+                    "text": "2",
+                    "correct": true
+                },
+                {
+                    "text": "1",
+                    "correct": false
+                }
+            ]
+        },
+        {
+            "text": "7-What color are often the domes of churches in Russia?",
+            "options": [
+                {
+                    "text": "silver",
+                    "correct": false
+                },
+                {
+                    "text": "Red",
+                    "correct": false
+                },
+                {
+                    "text": "Gold",
+                    "correct": true
+                }
+            ]
+        },
+        {
+            "text": "8-In which city is the famous Manneken Pis fountain?",
+            "options": [
+                {
+                    "text": "Amsterdam",
+                    "correct": false
+                },
+                {
+                    "text": "Brussels",
+                    "correct": true
+                },
+                {
+                    "text": "Paris",
+                    "correct": false
+                }
+            ]
+        },
+        {
+            "text": "9-What used to be the currency of Italy?",
+            "options": [
+                {
+                    "text": "Lira",
+                    "correct": true
+                },
+                {
+                    "text": "Euro",
+                    "correct": false
+                },
+                {
+                    "text": "Mark",
+                    "correct": false
+                }
+            ]
+        },
+        {
+            "text": "10-Where does a car with the letters CH come from?",
+            "options": [
+                {
+                    "text": "UK",
+                    "correct": false
+                },
+                {
+                    "text": "US",
+                    "correct": false
+                },
+                {
+                    "text": "Switzerland",
+                    "correct": true
+                }
+            ]
+        }
+    ]
+}`);
--- main.js
+++ main.js
@@ -1,35 +1,72 @@
+//Global Variables
+var arr_correct = new Array();
 
+//Called function while creating the form
+function createQuiz(){
+    var id = 0;
+
+    game.questions.forEach(element => {
+        var newp = document.createElement("p");
+        newp.id = "question" + (++id);
+        newp.innerHTML = element.text;
+        document.getElementById("quizPage").appendChild(newp);
+
+        element.options.forEach(element1 => {
+            var container = document.createElement("div");
+
+            var newi = document.createElement("input");
+            newi.id = "option" + (id);
+            newi.name = "option" + (id);
+            newi.type = "radio";
+            newi.value = element1.text;
+            if(element1.correct){
+                arr_correct.push(element1.text);
+            }
+               
+            container.appendChild(newi);
+            container.append(element1.text);
+            
+            document.getElementById("quizPage").appendChild(container);
+        });
+    });
+}
+
+//function is called after finish button is clicked
 function check(){
-    var answer1 = document.quiz.answer1.value;
-    var answer2 = document.quiz.answer2.value;
-    var answer3 = document.quiz.answer3.value;
-    var correct = 0;
-
-    if((answer1 == "Ankara") || (answer1 == "ankara"))
-        correct++;
-    if(answer2 == "Kebap")
-        correct++;
-    if(answer3 == "Bodrum")
-        correct++;
-
-    var messages = ["Great Job!", "Meh it is okay", "You need to do it better!"];
-    var img = ["img/success.gif", "img/meh.gif", "img/lose.gif"]; 
-
+    var score = 0;
     var range;
-    if(correct < 1){
+    
+    for(var i = 0; i < game.questions.length; i++){
+        var x = document.getElementsByName("option" + (i+1));
+        x.forEach(e => {
+            if(e.checked){
+                if(e.value == arr_correct[i]){
+                    score++;
+                }
+            }
+        });  
+    }
+   
+    var messages = ["Woooaw!!! Great Job!", "It is good!", "Meh it is okay, need more!", "You need to do it better!"];
+    var img = ["img/success.gif","img/good.gif", "img/meh.gif", "img/lose.gif"]; 
+
+    if(score < 3){
+        range = 3;
+    }
+    if(score >= 3 && score < 6){
         range = 2;
     }
-    if(correct > 0 && correct < 3){
+    if(score >= 6 && score < 9){
         range = 1;
     }
-    if(correct > 2){
+    if(score >= 9){
         range = 0;
     }
 
     document.getElementById("after_submit").style.visibility = "visible";
     
     document.getElementById("message").innerHTML = messages[range];
-    document.getElementById("number_correct").innerHTML = "You got " + correct + " answer correctly.";
+    document.getElementById("number_correct").innerHTML = "You got " + score + " answer correctly.";
     document.getElementById("img").style.width = "400px";
     document.getElementById("img").src = img[range];
 }
