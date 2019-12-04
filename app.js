6efb30dddcf44fbbba3a3c1ea10ce88ccb03192a
Sevil Coskun <sevilcoskun7@gmail.com>
2019-12-04 12:27:54 +0100

    make multi-user online users page and hold it

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 //include nodejs modules
 const express = require('express');
 var app = express();
-//var server = require('http').Server(app);
-//var io = require('socket.io')(server);
 const port = 8080;
 
 app.use(express.static(__dirname + '/public'));//mapping the public dir to client
@@ -16,27 +14,6 @@ app.listen(port, function (err, res) {
 	}
 });
 
-//app.listen is not working here
-/*server.listen(port, function (err, res) {
-	if (err) {
-		console.log("sever error");
-	}
-	else {
-		console.log("server srtarted");
-	}
-});
-
-app.get('/', function(req, res){
-	res.sendFile(__dirname + '/public/index.html');
-});
-
-io.on('connection', function(socket){
-	socket.emit('news', {hello: 'worrld'});
-	socket.on('my other event', function(data){
-		console.log(data);
-	});
-});*/
-
 var players = [];
 
 app.post('/login', function login(req,res){
@@ -45,21 +22,12 @@ app.post('/login', function login(req,res){
 		res.send("That nickname has already taken, please select new one!");
 	}
 	 else {
-		/* console.log("Add new player: " + req.query.nickname);
-		//create a new player
-		var player = {
-			'name': req.query.nickname,
-			'type': 0,
-			'playing': false,
-			'score': 0
-		};
-		players.push(player); */
 		res.status(200);
 		res.send(req.query.nickname);
 	} 
 });
 
-app.post('/multiPlayer', function multpPlayer(req, res){
+app.post('/multiPlayer', function multiPlayer(req, res){
 	console.log("Add new multi-player: " + req.query.nickname);
 		//create a new player
 		var player = {
@@ -76,6 +44,19 @@ app.post('/multiPlayer', function multpPlayer(req, res){
 app.get('/multiPlayer', function multiPlayer(req, res){
 	res.send(JSON.stringify(players));
 });
+
+app.delete('/deletePlayer', function deletePlayer(req, res){
+	let p;
+	console.log("Players ", players , req.query.nickname);
+	p = players.findIndex(item => item.name == req.query.nickname);
+	console.log("position " + p);
+	if(p > -1){
+		players.splice(p,1);
+	}
+	//item = players.filter(item => item.name == nickname);
+})
+
+
 
 
 
